4410254b12d6cbf047f7fd5e1a4c2ab4fcd423ba
mateicrainiceanu <100783062+mateicrainiceanu@users.noreply.github.com>
2024-05-28 18:51:30 +0300

    Sonet view done

--- src/dash/SonetLg.jsx
+++ src/dash/SonetLg.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
 import {getProxyy} from "../App";
+import {Button} from "@mui/material";
 
 function SonetLg() {
 	const {id} = useParams();
@@ -24,9 +25,26 @@ function SonetLg() {
 			<h1 className="text-xl text-center text-emerald-400 font-bold">Sonet</h1>
 			<hr className="my-2" />
 
-			{data && <div>
-        Pentru
-        </div>}
+			{data && (
+				<div>
+					<div className="text-emerald-300 text-center my-10">
+						<p>De la: </p>
+						<p className="font-bold">{data.fromUserId.name}</p>
+					</div>
+
+					<div className="p-5 bg-zinc-600 my-10 text-emerald-300">
+						<p>{data.mesaj}</p>
+					</div>
+					<h2 className="text-lg text-emerald-300 italic text-right">
+						Pentru: <span className="font-bold">{data.forName}</span>
+					</h2>
+					<div className="my-10">
+						<Button className="w-full " variant="outlined" color="primary">
+							Adauga la sonete pentru mine...
+						</Button>
+					</div>
+				</div>
+			)}
 		</div>
 	);
 }
