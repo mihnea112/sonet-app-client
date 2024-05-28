1670b40654579de881b4f5a3716b9b7edd744f78
mihnea112 <ayanna.mihnea@yahoo.com>
2024-05-28 12:24:50 +0300

    Fixes+Qr beta

--- src/dash/Dash.jsx
+++ src/dash/Dash.jsx
@@ -4,15 +4,16 @@ import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
 import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
-import TextField from "@mui/material/TextField";
-import Autocomplete from "@mui/material/Autocomplete";
 import Balls from "../home/Balls";
 import { Button, ButtonGroup } from "@mui/material";
 import { getProxyy } from "../App";
 import axios from "axios";
+import SearchComponent from "../components/SearchComponent";
+import QRCode from "react-qr-code";
 
 function Dash() {
   const [sonet, setSonet] = useState([]);
+  const [search, setSearch] = useState("");
   const [filter, setFilter] = useState("all");
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -23,16 +24,16 @@ function Dash() {
   }, []);
   function filterSonet(son) {
     console.log(filter);
-      console.log(son.mesaj !== undefined);
-    if (filter == "from") return (!son.fromName && son.mesaj);
-    else if (filter == "mesaj") return (!son.mesaj);
+    console.log(son.mesaj !== undefined);
+    if (filter == "from") return !son.fromName && son.mesaj;
+    else if (filter == "mesaj") return !son.mesaj;
     else return true;
   }
   return (
     <section className="max-w-xl mx-auto py-3">
       <Balls></Balls>
       <div className="text-center">
-      <h1
+        <h1
           className="text-5xl text-center md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4 text-emerald-300"
           data-aos="zoom-y-out"
         >
@@ -40,6 +41,10 @@ function Dash() {
         </h1>
         {sonet.lenght != 0 && (
           <div>
+            <SearchComponent
+              search={search}
+              setSearch={setSearch}
+            ></SearchComponent>
             <div className="my-3">
               <ButtonGroup variant="outlined" aria-label="Basic button group">
                 <Button onClick={() => setFilter("all")}>All</Button>
@@ -49,6 +54,10 @@ function Dash() {
             </div>
             {sonet
               .filter(filterSonet)
+              .filter(
+                (son) =>
+                  son.fromName?.includes(search) || son.mesaj?.includes(search)
+              )
               .map((son, i) => (
                 <div className="m-3">
                   <Accordion>
@@ -57,7 +66,7 @@ function Dash() {
                       aria-controls="panel1-content"
                       id="panel1-header"
                     >
-                      <Typography>
+                      <Typography className="text-emerald-300">
                         <div className="flex gap-4">
                           {son.fromName ? (
                             <h3 className="font-bold">{son.fromName}</h3>
@@ -74,12 +83,26 @@ function Dash() {
                         </div>
                       </Typography>
                     </AccordionSummary>
-                    <AccordionDetails>
-                      <div key={i}>
+                    <AccordionDetails className="bg-zinc-500">
+                      <div key={son._id} className="p-2">
                         {son.fromName && <h3>{son.fromName}</h3>}
-                        <h4>Mesaj:</h4>
-                        <p>{son.mesaj}</p>
-                        <button>QR</button>
+                        <h3 className="font-bold text-emerald-300">Mesaj:</h3>
+                        <p className="text-emerald-100 italic">{son.mesaj}</p>
+                        <QRCode
+                          size={256}
+                          style={{
+                            height: "auto",
+                            maxWidth: "100%",
+                            width: "100%",
+                          }}
+                          value={
+                            "http://" +
+                            window.location.host +
+                            "/sonet/" +
+                            son._id
+                          }
+                          viewBox={`0 0 256 256`}
+                        />
                       </div>
                     </AccordionDetails>
                   </Accordion>
